859227650d80c980b18ee59dc87af60ef62068d4
Ansis Kalnyn <ansis.kalnyn@yandex.ru>
2023-10-14 23:04:49 +0300

    add Data classes -добавили идентификатор data перед class -у параметров установили val и убрали кастомный setter -в перем. liked скопировали класс Post и установили изм. likes.

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,26 +1,20 @@
 package ru.neotology
 
-class Post(
-    var id: Int,
+data class Post(
+    val id: Int,
     val authorId: Int,
     val authorName: String,
     val content: String,
     val published: Long,
-    likes: Int,
-) {
-    var likes = likes
-        set(value) {
-            if (value >= 0) {
-                field = value
-            }
-        }
-
-}
+    val likes: Int,
+)
 
 fun main() {
 
     val post = Post(1, 1, "author", "content", likes = 0, published = 0)
-    post.likes = -100
-    println(post.likes)
+    val liked = post.copy(likes = post.likes + 1) //создаем копию класса и указываем только измененные параметры!
+    //data классы предоставляют встроеннуюб функцию преобразования в строку
+    //Post(id=1, authorId=1, authorName=author, content=content, published=0, likes=1)
+    println(liked)
 
 }
